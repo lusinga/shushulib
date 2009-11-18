2de240ef6e39bebe0aa5a7d7cc5db8bee8238f9f
Lusing <Lusing@14ead560-cc78-11de-937e-2741d7931076>
2009-11-18 16:41:31 +0000

    加入地支的六合，三合，六冲算法。

---
+++ trunk/src/lib/DiZhi.cpp
@@ -0,0 +1,29 @@
+#include "DiZhi.h"
+
+
+DiZhi::DiZhi(void)
+{
+}
+
+
+DiZhi::~DiZhi(void)
+{
+}
+
+
+bool DiZhi::he(int he1, int he2)
+{
+	return (he1 + he2) % 12 == 1;
+}
+
+
+bool DiZhi::sanhe(int he1, int he2, int he3)
+{
+	return ((he1 +4) % 12 == he2) && ((he2 +4) % 12 == he3);
+}
+
+
+bool DiZhi::chong(int chong1, int chong2)
+{
+	return (chong1 + 6) % 12 == chong2;
+}
