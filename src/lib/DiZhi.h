9934d8c6e65340a879614cc726963da1b85b4b80
Lusing <Lusing@14ead560-cc78-11de-937e-2741d7931076>
2009-11-18 16:41:31 +0000

    加入地支的六合，三合，六冲算法。

---
+++ src/lib/DiZhi.h
@@ -0,0 +1,23 @@
+#pragma once
+class DiZhi
+{
+public:
+	static const int DZzi   = 0;
+	static const int DZchou = 1;
+	static const int DZyin  = 2;
+	static const int DZmao  = 3;
+	static const int DZchen = 4;
+	static const int DZsi   = 5;
+	static const int DZwu   = 6;
+	static const int DZwei  = 7;
+	static const int DZshen = 8;
+	static const int DZyou  = 9;
+	static const int DZxu   = 10;
+	static const int DZhai  = 11;
+
+	DiZhi(void);
+	virtual ~DiZhi(void);
+	static bool he(int he1, int he2);//ÁùºÏ
+	static bool sanhe(int he1, int he2, int he3);//ÈýºÏ
+	static bool chong(int chong1, int chong2);//³å
+};
