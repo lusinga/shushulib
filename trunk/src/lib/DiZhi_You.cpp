83e9e4798976501567a3efbb387281c1d08cf3a2
Lusing <Lusing@14ead560-cc78-11de-937e-2741d7931076>
2009-11-21 06:35:41 +0000

    地支这回全了.

---
+++ trunk/src/lib/DiZhi_You.cpp
@@ -0,0 +1,14 @@
+#include "DiZhi_You.h"
+
+DiZhi_You::DiZhi_You(void)
+{
+}
+
+DiZhi_You::~DiZhi_You(void)
+{
+}
+
+string DiZhi_You::getName()
+{
+	return "со";
+}
---
+++ trunk/src/lib/DiZhi_You.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "yin.h"
+#include "jin.h"
+#include "dizhi.h"
+
+class DiZhi_You :
+	public Yin, public Jin, public DiZhi	
+{
+public:
+	DiZhi_You(void);
+	~DiZhi_You(void);
+
+	virtual string getName();
+};
