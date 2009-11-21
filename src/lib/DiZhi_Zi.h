3b00c8e8c714a8b852a024f43d5f096fb13969cc
Lusing <Lusing@14ead560-cc78-11de-937e-2741d7931076>
2009-11-21 05:55:15 +0000

    增加“地支子”。

---
+++ src/lib/DiZhi_Zi.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "yang.h"
+#include "shui.h"
+#include "dizhi.h"
+
+class DiZhi_Zi :
+	public Yang, public Shui, public DiZhi	
+{
+public:
+	DiZhi_Zi(void);
+	~DiZhi_Zi(void);
+
+	virtual string getName();
+};
