7ea9f2d97d86f65abe035334deb69e4ead34f830
Lusing <Lusing@14ead560-cc78-11de-937e-2741d7931076>
2009-11-21 08:08:40 +0000

    从此版本开始支持天盘。

--- trunk/src/lib/LiuRen.cpp
+++ trunk/src/lib/LiuRen.cpp
@@ -1,3 +1,40 @@
 #include "liuren.h"
 
-;
+#include <iostream>
+
+using namespace std;
+
+void LiuRen::formatPan(DiZhi* pdz)
+{
+	DiZhi* dzs[12];
+	dzs[0] = pdz;
+	for(int i=1;i<12;i++)
+	{
+		dzs[i]= Month::buildZhi(pdz->getDzid() + i);
+	}
+
+	cout<<dzs[5]->getName()<<dzs[6]->getName()<<dzs[7]->getName()<<dzs[8]->getName()<<endl;
+	cout<<dzs[4]->getName()<<"    "<<dzs[9]->getName()<<endl;
+	cout<<dzs[3]->getName()<<"    "<<dzs[10]->getName()<<endl;
+	cout<<dzs[2]->getName()<<dzs[1]->getName()<<dzs[0]->getName()<<dzs[11]->getName()<<endl;
+
+	for(int i=1;i<12;i++)
+	{
+		delete dzs[i];
+		dzs[i] = NULL;
+	}
+
+}
+
+void LiuRen::diPan()
+{
+	DiZhi* pdz = new DiZhi_Zi();
+	formatPan(pdz);
+	delete pdz;
+	pdz = NULL;
+}
+
+void LiuRen::tianPan(int month, int time)
+{
+	formatPan(Month::buildZhi(Month::getYueJiang(month)-time));
+}
