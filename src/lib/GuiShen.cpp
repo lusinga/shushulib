e45acbb45ea4a6040ca55ed201aadf53983a741d
lusing <lusing@14ead560-cc78-11de-937e-2741d7931076>
2011-02-16 16:27:22 +0000

    增加对贵神的处理。

---
+++ src/lib/GuiShen.cpp
@@ -0,0 +1,15 @@
+#include "GuiShen.h"
+
+GuiShen::GuiShen(TianGan* pTG, bool isDay)
+{
+	//¼×Îì¸ýÅ£Ñò
+	if(pTG->getDzid() == TianGan::TGjia || pTG->getDzid() == TianGan::TGwu || pTG->getDzid() == TianGan::TGgeng)
+	{
+
+	}
+}
+
+
+GuiShen::~GuiShen(void)
+{
+}
