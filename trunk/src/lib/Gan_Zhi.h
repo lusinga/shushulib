3dddbf5fef6e1cd02ff60d589684e9811308e936
lusing <lusing@14ead560-cc78-11de-937e-2741d7931076>
2011-02-16 13:43:33 +0000

    加入对干支甲子的处理。

--- trunk/src/lib/Gan_Zhi.h
+++ trunk/src/lib/Gan_Zhi.h
@@ -1,11 +1,20 @@
 #pragma once
 #include "ganzhi.h"
 
+#include "Tiangan.h"
+#include "DiZhi.h"
+
 class Gan_Zhi :
 	public GanZhi
 {
 public:
-	Gan_Zhi(void);
+	Gan_Zhi(int iTG, int iDZ);
 	virtual ~Gan_Zhi(void);
-};
 
+	virtual int getXing();
+	
+	virtual string getName();
+
+	TianGan* pTG;
+	DiZhi* pDZ;
+};
