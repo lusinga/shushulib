6ebedeff96026070c9a0e39b820e089a10903440
lusing <lusing@14ead560-cc78-11de-937e-2741d7931076>
2011-02-16 13:43:33 +0000

    加入对干支甲子的处理。

--- src/lib/Gan_Zhi.cpp
+++ src/lib/Gan_Zhi.cpp
@@ -1,11 +1,22 @@
 #include "Gan_Zhi.h"
+#include "Month.h"
 
-
-Gan_Zhi::Gan_Zhi(void)
+Gan_Zhi::Gan_Zhi(int iTG, int iDZ)
 {
+	this->pTG = Month::buildGan(iTG);
+	this->pDZ = Month::buildZhi(iDZ);
 }
-
 
 Gan_Zhi::~Gan_Zhi(void)
 {
+}
+
+int Gan_Zhi::getXing()
+{
+	return 0;
+}
+	
+string Gan_Zhi::getName()
+{
+	return this->pTG->getName()+this->pDZ->getName();
 }
