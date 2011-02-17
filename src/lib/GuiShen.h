9524016d5e6ce8882d8242b83c6ce61f4fdbfb57
lusing <lusing@14ead560-cc78-11de-937e-2741d7931076>
2011-02-17 13:11:50 +0000

    增加12贵神的名称。

--- src/lib/GuiShen.h
+++ src/lib/GuiShen.h
@@ -4,6 +4,19 @@
 class GuiShen
 {
 public:
+	static const int gsTianYi = 0;
+	static const int gsTengShe = 1;
+	static const int gsZhuQiu = 2;
+	static const int gsLiuHe = 3;
+	static const int gsGouChen =4;
+	static const int gsQingLong = 5;
+	static const int gsTianKong = 6;
+	static const int gsBaiHu = 7;
+	static const int gsTaiChang = 8;
+	static const int gsXuanWu = 9;
+	static const int gsTaiYin = 10;
+	static const int gsTianHou = 11;
+
 	GuiShen(TianGan* pTG, bool isDay);
 	~GuiShen(void);
 
