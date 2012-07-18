ccf98dca1ec1c90bc81d2a42bcfbe95bf57a0ba4
Lusing <Lusing@14ead560-cc78-11de-937e-2741d7931076>
2012-07-18 12:34:20 +0000

    为移植到Android上做准备

---
+++ trunk/src/lib/defaultFlags.h
@@ -0,0 +1,15 @@
+/*
+ * defaultFlags.h
+ *
+ *  Created on: 2011-8-21
+ *      Author: Louis Liu
+ */
+
+#ifndef DEFAULTFLAGS_H_
+#define DEFAULTFLAGS_H_
+
+//#define ANDROID_LIB
+
+//extern char* pCmdOut;
+
+#endif /* DEFAULTFLAGS_H_ */
