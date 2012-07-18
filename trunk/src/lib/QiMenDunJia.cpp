10842c93186b38cc5f93c9dbdb314c5ffb0a59c8
Lusing <Lusing@14ead560-cc78-11de-937e-2741d7931076>
2012-07-18 12:23:25 +0000

    添加奇门遁甲文件 增加阳遁和阴遁九局的判断

---
+++ trunk/src/lib/QiMenDunJia.cpp
@@ -0,0 +1,115 @@
+#include "QiMenDunJia.h"
+
+
+QiMenDunJia::QiMenDunJia(int jq)
+{
+	this->jieqi = jq;
+	this->setDunJu();
+}
+
+QiMenDunJia::~QiMenDunJia(void)
+{
+}
+
+void QiMenDunJia::setDunJu()
+{
+	isYangDun == (this->jieqi < JQXiaZhi && jieqi >= JQDongZhi);
+
+	switch(jieqi)
+	{
+	//—Ù∂›
+	case JQDongZhi:
+	case JQJingZhe:
+		dunju[0] = 1;
+		dunju[1] = 7;
+		dunju[2] = 4;
+		break;
+	case JQXiaoHai:
+		dunju[0] = 2;
+		dunju[1] = 5;
+		dunju[2] = 8;
+		break;
+	case JQDaHan:
+	case JQChunFen:
+		dunju[0] = 3;
+		dunju[1] = 9;
+		dunju[2] = 6;
+		break;
+	case JQMangZhong:
+		dunju[0] = 6;
+		dunju[1] = 3;
+		dunju[2] = 9;
+	break;
+	case JQGuYu:
+	case JQXiaoMan:
+		dunju[0] = 5;
+		dunju[1] = 2;
+		dunju[2] = 8;
+		break;
+	case JQLiChun:
+		dunju[0] = 8;
+		dunju[1] = 5;
+		dunju[2] = 2;
+		break;
+	case JQLiXia:
+	case JQQingMing:
+		dunju[0] = 4;
+		dunju[1] = 1;
+		dunju[2] = 7;
+		break;
+	case JQYuShui:
+		dunju[0] = 9;
+		dunju[1] = 6;
+		dunju[2] = 3;
+		break;
+		
+	//“ı∂›
+	case JQXiaZhi:
+	case JQBaiLu:
+		dunju[0] = 9;
+		dunju[1] = 3;
+		dunju[2] = 6;
+		break;
+	case JQXiaoShu:
+		dunju[0] = 8;
+		dunju[1] = 2;
+		dunju[2] = 5;
+		break;
+	case JQQiuFen:
+	case JQDaShu:
+		dunju[0] = 7;
+		dunju[1] = 1;
+		dunju[2] = 4;
+		break;
+	case JQLiQiu:
+		dunju[0] = 2;
+		dunju[1] = 5;
+		dunju[2] = 8;
+		break;
+	case JQShuangJiang:
+	case JQXiaoXue:
+		dunju[0] = 5;
+		dunju[1] = 8;
+		dunju[2] = 2;
+		break;
+	case JQDaXue:
+		dunju[0] = 4;
+		dunju[1] = 7;
+		dunju[2] = 1;
+		break;
+	case JQLiDong:
+	case JQHanLu:
+		dunju[0] = 6;
+		dunju[1] = 9;
+		dunju[2] = 3;
+		break;
+	case JQChuShu:
+		dunju[0] = 1;
+		dunju[1] = 4;
+		dunju[2] = 7;
+		break;
+
+	default:
+		break;
+	}
+}
