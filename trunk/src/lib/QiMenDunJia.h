5f8720627c66326971ba1a5cb78b846f7d52aa8b
Lusing <Lusing@14ead560-cc78-11de-937e-2741d7931076>
2012-08-26 11:18:46 +0000

    测试地盘功能

--- trunk/src/lib/QiMenDunJia.h
+++ trunk/src/lib/QiMenDunJia.h
@@ -1,11 +1,16 @@
 #include "Month.h"
 
+#include "Gua.h"
+
+const int qiMenDiPan[9] = {BG_Kan, BG_Qian, BG_Dui, BG_Kun, BG_Li, BG_Xun, BG_Zhen, BG_Gen, BG_Zhong};
+
 #pragma once
 class QiMenDunJia
 {
 public:
 	QiMenDunJia(int jieqi);
 	~QiMenDunJia(void);
+	static void printDiPan();
 
 private:
 	int jieqi;
