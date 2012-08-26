34e3cadbc426ad2d40dc69da218399b20e8a495b
Lusing <Lusing@14ead560-cc78-11de-937e-2741d7931076>
2012-08-26 11:19:35 +0000

    六仪三奇

---
+++ src/lib/QiYi.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <boost/shared_ptr.hpp>
+#include <string>
+#include <boost/make_shared.hpp>
+
+#include "Gua.h"
+
+const int ZhiFu    = 0;
+const int JiuTian  = 1;
+const int JiuDi    = 2;
+const int ZhuQue   = 3;
+const int GouChen  = 4;
+const int LiuHe    = 5;
+const int LiuYin   = 6;
+const int TengShe  = 7;
+
+const char BaZhaShenName[8][6] = {"Ö±·û","¾ÅÌì","¾ÅµØ","ÖìÈ¸","¹´³Â","ÁùºÏ","ÁùÒõ","ÎŸÉß"};
+
+class BaZhaShen
+{
+public:
+	static inline boost::shared_ptr<std::string> getBaMenName(int men)
+	{
+		return boost::make_shared<std::string>(BaZhaShenName[men%8]);
+	}
+
+};
