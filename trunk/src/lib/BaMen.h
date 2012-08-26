3626bddc7f59e5ff6133e4f36d5beaabfd65a352
Lusing <Lusing@14ead560-cc78-11de-937e-2741d7931076>
2012-08-26 11:37:50 +0000

    按天盘顺序调整八门值

--- trunk/src/lib/BaMen.h
+++ trunk/src/lib/BaMen.h
@@ -7,16 +7,16 @@
 #include "Gua.h"
 
 const int XiuMen   = 0;
-const int ShengMen = 1;
-const int ShangMen = 2;
-const int DuMen    = 3;
+const int ShengMen = 7;
+const int ShangMen = 6;
+const int DuMen    = 5;
 const int Jing3Men = 4;
-const int SiMen    = 5;
-const int Jing1Men = 6;
-const int KaiMen   = 7;
+const int SiMen    = 3;
+const int Jing1Men = 2;
+const int KaiMen   = 1;
 
-const char BaMenName[8][6] = {"休门","生门","伤门","杜门","景门","死门","惊门","开门"};
-const int BaMenGua[8] = {BG_Kan, BG_Gen, BG_Zhen, BG_Xun, BG_Li, BG_Kun, BG_Dui, BG_Qian};
+const char BaMenName[8][6] = {"休门","开门","惊门","死门","景门","杜门","伤门","生门"};
+const int BaMenGua[8] = {BG_Kan,BG_Qian,BG_Dui,BG_Kun,BG_Li,BG_Xun,BG_Zhen,BG_Gen};
 
 class BaMen
 {
