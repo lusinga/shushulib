fb82b3243a1932b7232024947097f1e3454e011a
Lusing <Lusing@14ead560-cc78-11de-937e-2741d7931076>
2012-08-26 09:25:57 +0000

    开始支持八卦九宫

---
+++ trunk/src/lib/Gua.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Month.h"
+
+#include <boost/shared_ptr.hpp>
+
+const int BG_Kan   = 1;
+const int BG_Kun   = 2;
+const int BG_Zhen  = 3;
+const int BG_Xun   = 4;
+const int BG_Qian  = 6;
+const int BG_Dui   = 7;
+const int BG_Gen   = 8;
+const int BG_Li    = 9;
+
+const int BG_Zhong = 5;//中五宫，不属于八卦之一
+
+const char GuaName[9][4] = {"坎","坤","震","巽","中","乾","兑","艮","离"};   
+
+class BaGua
+{
+public:
+	static boost::shared_ptr<string> getGuaName(int gua);
+};
