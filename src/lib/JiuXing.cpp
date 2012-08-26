bd4ed021166b5d8d5d4fda0c343fd1f0cd4ddb23
Lusing <Lusing@14ead560-cc78-11de-937e-2741d7931076>
2012-08-26 09:40:21 +0000

    九星加入别名

--- src/lib/JiuXing.cpp
+++ src/lib/JiuXing.cpp
@@ -9,6 +9,11 @@ boost::shared_ptr<std::string> JiuXing::getXingName(int jiu_xing)
 	return boost::make_shared<std::string>(JiuXingName[jiu_xing%9]);
 }
 
+boost::shared_ptr<std::string> JiuXing::getXingAnotherName(int jiu_xing)
+{
+	return boost::make_shared<std::string>(JiuXingAnotherName[jiu_xing%9]);
+}
+
 int JiuXing::getStaticGua(int jiu_xing)
 {
 	return staticPosition[jiu_xing % 9];
--- src/lib/JiuXing.h
+++ src/lib/JiuXing.h
@@ -16,12 +16,14 @@ const int TianYingXing  = 7;
 const int TianZhuXing   = 8;
 
 const char JiuXingName[9][10] = {"天辅星","天禽星","天心星","天冲星","天任星","天蓬星","天芮星","天英星","天柱星"};
+const char JiuXingAnotherName[9][10] = {"武曲星","廉贞星","文曲星","破军星","英明星","右弼星","左辅星","贪狼星","禄存星"};
 const int staticPosition[9] = {BG_Xun, BG_Zhong, BG_Qian, BG_Zhen, BG_Gen, BG_Kan, BG_Kun, BG_Li, BG_Dui};
 
 class JiuXing
 {
 public:
 	static boost::shared_ptr<std::string> getXingName(int jiu_xing);
+	static boost::shared_ptr<std::string> getXingAnotherName(int jiu_xing);
 	//获得九星在八卦配合时的原位
 	static int getStaticGua(int jiu_xing); 
 };
